d8dace8489328c403a3d3dd28e982435ca2a9a2b
Egor <egormolchanov1401@gmail.com>
2022-07-18 12:20:51 +0300

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Egor Molchanov on 7/18/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
